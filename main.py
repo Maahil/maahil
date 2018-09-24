b045bab77bdd645ece5b5d83fa3ddf3a44a45475
Maahil <43535123+Maahil@users.noreply.github.com>
2018-09-24 14:28:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+num=int(input())
+if num>0:
+    print("Positive")
+elif num==0:
+    print("Zero")
+else:
+    print("Negative")
